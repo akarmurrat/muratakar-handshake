562da893cd29d855ea7dd329a8f4f330c055ece9
Murat Akar <akarmurrat@gmail.com>
2025-08-13 23:18:38 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
-module.exports = nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+  // output: 'export'  // BUNU KULLANMA
+}
+module.exports = nextConfig
